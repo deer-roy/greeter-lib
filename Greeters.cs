bdb52a93ea43b83cee9f517f3f2226abdb47024b
Roy <roy.motloutsi@gmail.com>
2023-11-02 17:36:46 +0200

    initial commit

---
+++ Greeters.cs
@@ -0,0 +1,22 @@
+﻿namespace DoSomethingLib;
+
+public interface IGreeter
+{
+    string Greet(string name);
+}
+
+public class HelloGreeter : IGreeter
+{
+    public string Greet(string name)
+    {
+        return $"Hello {name}.";
+    }
+}
+
+public class AhoyGreeter : IGreeter
+{
+    public string Greet(string name)
+    {
+        return $"Ahoy there, {name}.";
+    }
+}
